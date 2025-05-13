ed7d5a49a759f561d8310d885d2ed5bab453d8b7
22crystyle <22crystyle@gmail.com>
2025-05-13 13:14:06 +0300

    fix: Очистил CardController

--- src/main/java/com/example/bankcards/controller/CardController.java
+++ src/main/java/com/example/bankcards/controller/CardController.java
@@ -2,8 +2,6 @@ package com.example.bankcards.controller;
 
 import com.example.bankcards.dto.CardMapper;
 import com.example.bankcards.service.CardService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +15,5 @@ public class CardController {
     public CardController(CardMapper cardMapper, CardService cardService) {
         this.cardMapper = cardMapper;
         this.cardService = cardService;
-    }
-
-    @GetMapping("/{id}")
-    public String getBankCard(@PathVariable int accountId, @PathVariable int id) {
-        return "Bank card controller";
     }
 }
