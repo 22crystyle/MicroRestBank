cefa972b3d380f5b31981f97b955281f43abc1f5
22crystyle <22crystyle@gmail.com>
2025-06-02 18:14:47 +0300

    TODO: todoes

--- src/main/java/com/example/bankcards/service/AccountService.java
+++ src/main/java/com/example/bankcards/service/AccountService.java
@@ -23,16 +23,17 @@ public class AccountService {
     private final PasswordEncoder encoder;
     private final RoleRepository roleRepository;
 
-    public Account createAccount(AccountRequest request) {
+    @Transactional
+    public Account createAccount(AccountRequest request) { // TODO: заранее проверить уникален ли `username` через `repository.existsByUsername(request.username())`
         Account account = mapper.toEntity(request);
         Role defaultRole = roleRepository.findById(request.role_id())
-                .orElseThrow(() -> new IllegalArgumentException("Role not found"));
+                .orElseThrow(() -> new IllegalArgumentException("Role not found")); // TODO: RoleNotFoundException
         account.setPassword(encoder.encode(request.password()));
         account.setRole(defaultRole);
-        repository.save(account);
-        return account;
+        return repository.save(account);
     }
 
+    @Transactional
     public boolean deleteById(Long id) {
         if (!repository.existsById(id)) {
             return false;
@@ -43,7 +44,7 @@ public class AccountService {
 
     @Transactional(readOnly = true)
     public Account getAccountById(Long id) {
-        return repository.findById(id).orElseThrow(() -> new UsernameNotFoundException("Account not found by id: " + id));
+        return repository.findById(id).orElseThrow(() -> new UsernameNotFoundException("Account not found by id: " + id)); // TODO: AccountNotFoundException
     }
 
     @Transactional(readOnly = true)
