30e5e53227bb6d48cb54f6da79fbd90d27995bf6
22crystyle <22crystyle@gmail.com>
2025-08-16 12:52:15 +0300

    fix: CardStatus no more contains list of cards

--- services/card-service/src/main/java/com/example/bankcards/entity/CardStatus.java
+++ services/card-service/src/main/java/com/example/bankcards/entity/CardStatus.java
@@ -1,10 +1,7 @@
 package com.example.bankcards.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
-
-import java.util.List;
 
 @Entity
 @Table(name = "card_statuses")
@@ -16,12 +13,6 @@ public class CardStatus {
     @Column(unique = true, nullable = false)
     private String name;
     private String description;
-    @OneToMany(
-            mappedBy = "status",
-            fetch = FetchType.EAGER
-    )
-    @JsonIgnore
-    private List<Card> cards;
 
     public CardStatusType getCardStatusType() {
         return CardStatusType.valueOf(name);
